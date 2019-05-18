0c1bb615300a577b609160f660de9fd3c9c5d4c5
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2019-05-18 03:00:28 +0300

    constructor_example.c içerisindeki gereksiz yorum satırları çıkarıldı

--- src/constructor_example.c
+++ src/constructor_example.c
@@ -2,12 +2,6 @@
 void __attribute__((constructor)) func1(void);
 void __attribute__((destructor)) func2(void);
 
-//void func1(void);
-//void func2(void);
-//#pragma startup func1()
-//#pragma exit func2()
-
-// pragma startu'ın yapığı işi yapıyor
 int main(){
     printf("main\n");
     return 0;
